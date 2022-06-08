2d4e4e746d59e71113a796cf04acc1c2a9c6b8bf
DenSmith1 <ntreid80@gmail.com>
2022-06-08 19:11:14 +0400

    Домашнее задание. Рекурсия

---
+++ Ex07/Program.cs
@@ -0,0 +1,43 @@
+﻿// Домашнее задание 1. Показать натуральные числа от M до N
+
+// void ShowNumbersDown(int M, int N)
+// {
+//     if (M < N) return;
+//     Console.Write(M + " ");
+//     ShowNumbersDown(M - 1, N);
+// }
+
+// void ShowNumbersUp(int M, int N)
+// {
+//     if (M < N) return;
+//     ShowNumbersUp(M - 1, N);
+//     Console.Write(M + " ");
+// }
+
+// Console.Clear();
+// Console.Write("Введите натуральное число (M): ");
+// int M = int.Parse(Console.ReadLine() ?? "0");
+// Console.Write("Введите натуральное число (N): ");
+// int N = int.Parse(Console.ReadLine() ?? "0");
+
+// if (N >= M) ShowNumbersUp(N, M);
+// else ShowNumbersDown(M, N);
+
+
+
+// Домашнее задание 2. Найти сумму элементов от M до N, N и M заданы
+
+double ElementsSummary(int M, int N)
+{
+    double Sum = M;
+    if (M >= N) return Sum;
+    else return Sum + ElementsSummary(M + 1, N);
+}
+
+Console.Clear();
+Console.Write("Введите натуральное число (M): ");
+int M = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите натуральное число (N): ");
+int N = int.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine($"Сумма элементов от {M} до {N} = {ElementsSummary(M, N)}");
